dad9ad87ca62ee1d7a0b5b28819ec3436013635c
Vladimir <39942321+Vlad1m8r@users.noreply.github.com>
2020-09-14 02:07:15 +0500

    Create main.py

---
+++ Task-1/main.py
@@ -0,0 +1,61 @@
+import math
+from tkinter import *
+
+root = Tk()
+c = Canvas(root, width=600, height=600, bg="white")
+drBtn = Button(text="Направление")
+spLabel = Label(text="Скорость  1-100")
+spEntry = Entry()
+spEntry.insert(1, "50")
+spBtn = Button(text="Поменять скорость")
+
+cirlce = c.create_oval(100, 100, 500, 500, fill='red')  # 400 - диаметр. (300;300) - центр окружности
+ball = c.create_oval(70, 300, 100, 330, fill='green')
+
+drball = 1  # направление
+speed = 10
+
+def move(angle):
+    if angle >= 360:
+        angle = 0
+    x = 200 * math.cos(math.radians(angle))  # 200 - радиус
+    y = 200 * math.sin(math.radians(drball * angle))  # направление (+angle; -angle)
+    angle += 1
+    c.coords(ball, 285 + x, 285 + y, 315 + x, 315 + y)  # (300;300) - центр точки. 285;315 - ее границы
+    root.after(int(speed), move, angle)  # скорость
+
+
+def direc(event):  # направление
+    global drball
+    drball = drball * (-1)
+
+
+def chspeed(event):
+    global speed
+    new_sp = spEntry.get()
+    try:
+        if 1 <= int(new_sp) <= 100:
+            speed = 20 - int(new_sp) / 5
+            if speed < 1:
+                speed = 1
+            spEntry.delete(0, END)
+            spEntry.insert(1, new_sp)
+        else:
+            spEntry.delete(0, END)
+            spEntry.insert(1, "Неверная скорость")
+    except:
+        spEntry.delete(0, END)
+        spEntry.insert(1, "Неверная скорость")
+
+
+drBtn.bind('<Button-1>', direc)
+spBtn.bind('<Button-1>', chspeed)
+
+c.pack()
+drBtn.pack()
+spLabel.pack()
+spEntry.pack()
+spBtn.pack()
+
+root.after(10, move, 0)
+root.mainloop()
